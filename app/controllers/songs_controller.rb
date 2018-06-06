80ef87358a59e07be6d0da5da560661d270c0722
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-06 18:36:23 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,7 +8,7 @@ class SongsController < ApplicationController
   def upload
     binding.pry
     CSV.foreach(Rails.root.join('db','songs.csv'), headers: true) do |song|
-      Song.create(:title => song[0]).tap{|s| s.artist.build(:name => song[1])}
+      Song.create(:title => song[0]).tap{|s| s.artist.name = song[1]}
     end
     redirect_to songs_path
   end
